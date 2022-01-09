b5949f5cd51bc34204ffc3f06172961f86904ed9
zhonghf <zhonghf@winning.com.cn>
2022-01-09 19:27:12 +0800

    leetCode 1.8

---
+++ interview_oriented/interview-oriented-demo1/src/main/java/leetCode/Solution2.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+import org.junit.Test;
+
+/**
+ * @author kelvinchung
+ *
+ * 01.03. URL化
+ * URL化。编写一种方法，将字符串中的空格全部替换为%20。假定该字符串尾部有足够的空间存放新增字符，
+ * 并且知道字符串的“真实”长度。（注：用Java实现的话，请使用字符数组实现，以便直接在数组上操作。）
+ *
+ */
+public class Solution2 {
+
+    public String replaceSpaces(String S, int length) {
+
+//        String S = "Mr  Kelvin hahah       ";
+        // 取个长度
+        String temp = S.substring(0, length);
+        return temp.replace(" ", "%20");
+    }
+
+}
